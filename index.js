11da20440f69130c11fa3af34399d71a9f33fe49
Louis Orleans <louis@0rleans.com>
2015-07-12 16:02:47 -0700

    init

---
+++ index.js
@@ -0,0 +1,45 @@
+var WEBSITE_PORT = (process.env.PORT || 8080);
+
+var fs = require('fs');
+
+var express = require('express');
+var app = express();
+var server;
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+var data;
+
+module.exports = {
+    init: function () {
+        this.startServer();
+        return this;
+    },
+    startServer: function () {
+        app.post('/getData', function (req, res) {
+            res.send(JSON.stringify(data));
+        });
+        app.post('/setData', function (req, res) {
+            data = req.body.data
+        });
+
+        server = app.listen(WEBSITE_PORT, function () {
+            console.log('web server listening on *:' + WEBSITE_PORT);
+        });
+    },
+    stopServer: function () {
+        server.close();
+        return true;
+    },
+    getApp: function () {
+        return app;
+    },
+    getServer: function () {
+        return server;
+    }
+};
+
+module.exports.init();
